4624824895f40e615ed70c9e7fb801892da72379
raddrooster08@gmail.com <>
2026-07-08 17:02:14 -0400

    Added basic AI placeholder logic Added Basic AI Added Entity and Ore Spawners

---
+++ Assets/Scripts/BehaviorTrees/Node.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace BehaviorTree
+{
+    public enum ExecutionState { running, success, failure }
+    public abstract class Node
+    {
+        public Node parent;
+        public List<Node> children;
+
+        protected ExecutionState state;
+
+        /// <summary>
+        /// The current Execution state of the node
+        /// </summary>
+
+
+        /// <summary>
+        /// The method that runs every frame and evaluates the  current execution state
+        /// 
+        /// </summary>
+        public abstract ExecutionState Evaluate();
+
+    }
+}
